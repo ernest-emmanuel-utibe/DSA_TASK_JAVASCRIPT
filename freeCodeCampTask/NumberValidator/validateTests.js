a0a4347772d26a72965d0fa36083a9739e91c520
Ernest Emmanuel Utibe <110993733+morsecodes007@users.noreply.github.com>
2023-06-22 09:23:48 +0100

    Update validateTests.js

--- freeCodeCampTask/NumberValidator/validateTests.js
+++ freeCodeCampTask/NumberValidator/validateTests.js
@@ -4,17 +4,17 @@
         expect(telephoneCheck("1 (555) 555-5555")).toBe(true);
     });
 
-    // Tests that the function returns true for a valid US phone number without country code. 
+    // Tests that the function returns true for a valid US phone number without country code
     it("test_valid_us_phone_number_without_country_code", () => {
         expect(telephoneCheck("555-555-5555")).toBe(true);
     });
 
-    // Tests that the function returns false for an invalid phone number with incorrect format. 
+    // Tests that the function returns false for an invalid phone number with incorrect format
     it("test_invalid_phone_number_with_incorrect_format", () => {
         expect(telephoneCheck("555-5555")).toBe(false);
     });
 
-    // Tests that the function returns false for an invalid phone number with parentheses in incorrect places. 
+    // Tests that the function returns false for an invalid phone number with parentheses in incorrect places
     it("test_phone_number_with_parentheses_in_incorrect_places", () => {
         expect(telephoneCheck("(555)5(55?)-5555")).toBe(false);
     });
